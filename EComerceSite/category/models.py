4a1fb60e195de33c6a4a04c10489c71440a1407d
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-07-16 18:27:30 +0530

    context processor catagory navbar displaying

--- EComerceSite/category/models.py
+++ EComerceSite/category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -11,6 +11,10 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'caregory'
         verbose_name_plural = 'categories'
+
+
+    def get_url(self):
+        return reverse('product_by_category' ,args=[self.slug])
 
 
     def __str__(self):
