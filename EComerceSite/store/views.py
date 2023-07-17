a67d5cf701835057853230385380f10c2193c710
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-07-17 23:16:24 +0530

    cart management

--- EComerceSite/store/views.py
+++ EComerceSite/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import Product
 from .models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 # Create your views here.
 
 def store(request,category_slug=None):
@@ -38,11 +40,14 @@ def product_details(request ,category_slug,product_slug ):
     try:
         # category__slug mehema __ dekak dala apita puluwan vena app ekaka thiyenma model ekaka atribute ekak access karanna
         single_product = Product.objects.get(category__slug = category_slug , slug = product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request) , product = single_product).exists()
     except Exception as e:
         raise e
     
     context = {
-        'single_product':single_product
+        'single_product':single_product,
+        'in_cart' : in_cart,
+
     }
 
     return render(request , 'store/product_details.html' ,context)
