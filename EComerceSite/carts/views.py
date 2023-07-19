37d2e5b7d239dd1a210120c16fe9b67351aa8314
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-07-20 01:56:16 +0530

    cart item , variation , increment,dicrement

--- EComerceSite/carts/views.py
+++ EComerceSite/carts/views.py
@@ -14,6 +14,8 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    product = Product.objects.get(id = product_id) # get the product
+    product_variation= [] 
     if request.method == 'POST':
         for item in request.POST:
             key = item
@@ -21,16 +23,12 @@ def add_cart(request, product_id):
 
             try :
                 variation = Variation.objects.get(variation_category__iexact = key ,variation_value__iexact=value)
-                print(variation)
+                product_variation.append(variation)
+                # print(variation)
+                 
             except:
                 pass
-
         # return HttpResponse(color + ' '+ size)
-    
-    
-    
-    product = Product.objects.get(id = product_id) # get the product
-
     # cookies wala thiyena cart_id eka gaththa Cart eke id eka widiyata
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))#get the cart using the cart_id present in the session
@@ -38,7 +36,7 @@ def add_cart(request, product_id):
         cart = Cart.objects.create(
             cart_id = _cart_id(request)
         )
-        cart.save()
+    cart.save()
 
 # dan thiyenawa product eka saha cart eka (product , cart)
 # dan me product eka api danna ona cart eka athulata
@@ -46,36 +44,82 @@ def add_cart(request, product_id):
 # cartItem eke thiyenawa product / cart kiyala dekak
 # methanin gaththa product ,cart deka assign karanawa e cartItem eke dekata
 # antimeta url ekath wenas karanna ona
-    try:
-        cart_item = CartItem.objects.get(product =  product ,cart = cart)
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+    
+    
+    # mulinma api cartitem eken ena ekai api gawa thiyena product ekai cart ekai samanada balanawa
+    #  etapasse exists() kiyana eken gannawa eka True or False da kiyala
+    # assing karanawa is_cart_item_exists
+    is_cart_item_exists = CartItem.objects.filter(product= product , cart = cart).exists()
+    
+    # if ekak aragena True nm
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product =product ,cart = cart)
+        
+        #existing_variation --> database
+        # current variation --> product_variation
+        # item_id -> database  
+        
+        ex_var_list = []
+        id = []
+
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            # api list ekak widiyata ganna ona
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+
+        print(ex_var_list)
+        # if ekak dala
+        if product_variation in ex_var_list:
+            # return HttpResponse('true')
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product = product , id = item_id)
+            item.quantity += 1
+            print("kalina add nm meka wada karanne")
+            item.save()
+        else:
+            item = CartItem.objects.create(product = product,quantity =1 , cart =cart)
+            # return HttpResponse('false')
+            # methana product variation eka empty da kiyala balanawa me if eken
+            if len(product_variation) >0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+                print("aluthen add nm meka wada")
+                item.save()
+    else :
         cart_item = CartItem.objects.create(
             product = product,
             quantity = 1,
             cart = cart,
         )
-        cart_item.save()
+        if len(product_variation) >0:
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
+            print("danne na ai kiyala")
+            cart_item.save()
     return redirect('cart')
 
 
-def remove_cart(request , product_id):
+def remove_cart(request , product_id ,cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product , id = product_id)
-    cart_item = CartItem.objects.get(product = product , cart = cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product = product , cart = cart ,id = cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect('cart')
 
 
-def remove_cart_item(request , product_id):
+def remove_cart_item(request , product_id ,cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product , id = product_id)
-    cart_item = CartItem.objects.get(product = product , cart = cart)
+    cart_item = CartItem.objects.get(product = product , cart = cart , id = cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
