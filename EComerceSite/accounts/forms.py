6a955615fabe1bfe38d31142221cbfbbb196031c
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-07-20 20:16:42 +0530

    check password and errors handling

--- EComerceSite/accounts/forms.py
+++ EComerceSite/accounts/forms.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django import forms 
 from .models import Account
 
@@ -6,17 +7,17 @@ class RegistrationForm(forms.ModelForm):
         'placeholder': 'Enter Password',
         'class': 'form-control',
     }))
-
     Confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Enter Password'
     }))
-
     # methana normal user kenekta ona dewal witharai add karnne
     # me meta data tika mulinma balanawa normal userge dewalda kiyala 
     class Meta:
         model = Account
         fields = ['first_name','last_name','email','password','phone_number']
         # fields = ['first_name','last_name','email','password']
+
+
 
 
     # apita loop ekak through serama atribute walata css add karanna puluwan
@@ -29,3 +30,17 @@ class RegistrationForm(forms.ModelForm):
         
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+        # apita puluwan password ekai comfirm password ekai check karanna
+    def clean(self):
+        print("password")
+        cleaed_date =super(RegistrationForm,self).clean()
+        password = cleaed_date.get('password')
+        confirm_password = cleaed_date.get('Confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
