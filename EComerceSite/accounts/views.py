e4869aad42ade41c14eee4ea1fe85e66e9c02880
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-07-31 12:33:43 +0530

    make dash board

--- EComerceSite/accounts/views.py
+++ EComerceSite/accounts/views.py
@@ -15,6 +15,7 @@ from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+from orders.models import Order
 
 from carts.views import _cart_id
 from carts.models import Cart ,CartItem
@@ -169,7 +170,12 @@ def activate(request ,uidb64 ,token):
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request ,'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id = request.user.id ,is_ordered = True)
+    orders_count = orders.count()
+    context = {
+        'orders_count' : orders_count,
+    }
+    return render(request ,'accounts/dashboard.html' ,context)
 
 def forgetpassword(request):
     return render(request , 'accounts/forgetpassword.html')
